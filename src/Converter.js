6250f0825a3dc2f090bd398956c3b5b958e44f17
Sibylle Sehl <sibylle.sehl@googlemail.com>
2018-03-11 16:10:39 +0000

    Implement function and pass first test

---
+++ src/Converter.js
@@ -0,0 +1,13 @@
+/** Unit Converter **/
+function convertFahrenheitToCelsius(temperature) {
+  return ((temperature - 32)/1.8).toFixed(1);
+}
+
+//function convertCelsiusToFahrenheit(temperature) {
+//  return (temperature*1.8) + 32;
+//}
+
+// normal formula from celsius to Fahrenheit conversion is
+// celsius = (fahrenheit - 32)/1.8
+
+// (celsius * 1.8) + 32 = fahrenheit
