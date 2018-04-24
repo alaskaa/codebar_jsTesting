fd70dfe9224d1d46971f203829ad5c0559abded9
Sibylle Sehl <sibylle.sehl@googlemail.com>
2018-04-24 20:04:49 +0100

    first own test implemented and passing

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -1,10 +1,15 @@
-describe("Cooking Ingredients Converter", function() {
-
-  describe("Temperatures", function() {
-    it("Fahrenheit to Celsius", function() {
-      var result = convertFahrenheitToCelsius(100);
+describe('Cooking Ingredients Converter', function() {
+  describe('Temperatures', function() {
+    it('Fahrenheit to Celsius', function() {
+      let result = convertFahrenheitToCelsius(100);
 
       expect(result).toEqual('37.8');
+    });
+
+    it('Celsius to Fahrenheit', function() {
+      let result = convertCelsiusToFahrenheit(37.8);
+
+      expect(result).toEqual('100');
     });
   });
 });
--- src/Converter.js
+++ src/Converter.js
@@ -1,11 +1,11 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
-  return ((temperature - 32)/1.8).toFixed(1);
+  return ((temperature - 32) / 1.8).toFixed(1);
 }
 
-//function convertCelsiusToFahrenheit(temperature) {
-//  return (temperature*1.8) + 32;
-//}
+function convertCelsiusToFahrenheit(temperature) {
+  return (temperature * 1.8 + 32).toFixed(0);
+}
 
 // normal formula from celsius to Fahrenheit conversion is
 // celsius = (fahrenheit - 32)/1.8
